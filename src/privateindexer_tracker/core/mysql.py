ef0c2c2a5c5d70c737a85822570fe2ddcd238a3d
David Hume <xenophilicy@gmail.com>
2025-12-29 14:30:47 -0600

    chore: add code comments

--- src/privateindexer_tracker/core/mysql.py
+++ src/privateindexer_tracker/core/mysql.py
@@ -11,14 +11,22 @@ _db_pool: Optional[aiomysql.Pool] = None
 
 
 async def connect_database():
+    """
+    Creates a connection pool to MySQL database
+    """
     global _db_pool
     _db_pool = await aiomysql.create_pool(host=MYSQL_HOST, port=MYSQL_PORT, user=MYSQL_USER, password=MYSQL_PASSWORD, db=MYSQL_DB, autocommit=True)
     log.debug("[MYSQL] Connected to database")
+
+    # create the task for background execution
     asyncio.create_task(background_worker())
     log.debug("[MYSQL] Started background worker task")
 
 
 async def disconnect_database():
+    """
+    Closes MySQL connection pool if active
+    """
     if _db_pool is not None:
         _db_pool.close()
         await _db_pool.wait_closed()
@@ -26,6 +34,9 @@ async def disconnect_database():
 
 
 async def background_worker():
+    """
+    Executes MySQL queries asynchronously to prevent request congestion
+    """
     while True:
         query, params = await background_updates.get()
         try:
@@ -37,6 +48,9 @@ async def background_worker():
 
 
 async def _with_retry(fn, *args, **kwargs):
+    """
+    Retries failed MySQL queries up to MYSQL_MAX_RETY times
+    """
     for attempt in range(1, MYSQL_MAX_RETY + 1):
         try:
             return await fn(*args, **kwargs)
@@ -53,6 +67,10 @@ async def _with_retry(fn, *args, **kwargs):
 
 
 async def fetch_all(query: str, params: tuple = ()):
+    """
+    Execute a query to MySQL and fetch all rows
+    """
+
     async def _do():
         async with _db_pool.acquire() as conn:
             async with conn.cursor(aiomysql.DictCursor) as cur:
@@ -63,6 +81,10 @@ async def fetch_all(query: str, params: tuple = ()):
 
 
 async def fetch_one(query: str, params: tuple = ()):
+    """
+    Execute a query to MySQL and fetch a single row
+    """
+
     async def _do():
         async with _db_pool.acquire() as conn:
             async with conn.cursor(aiomysql.DictCursor) as cur:
@@ -73,6 +95,10 @@ async def fetch_one(query: str, params: tuple = ()):
 
 
 async def execute(query: str, params: tuple = (), include_row_id: bool = False, include_row_count: bool = False):
+    """
+    Execute a query to MySQL and optionally fetch the row ID and modified row count
+    """
+
     async def _do():
         async with _db_pool.acquire() as conn:
             async with conn.cursor() as cur:
